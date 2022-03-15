dbddce7660ff55c5dc2905e8c2ac903a33748cde
Sovietsupporter69 <84580170+Sovietsupporter69@users.noreply.github.com>
2022-03-15 09:19:06 +0000

    Created AR page

---
+++ AR.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <title> Fill in the form! </title>
+</head>
+<body BGcolor="#232389">
+  <a-scene>
+    <a-gltf-model position="0 0 -4" rotation="0 0 0" scale="1 1 1" src="Donut/Donut.gltf"></a-gltf-model>
+    
+    <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+    <a-sky color="#ECECEC"></a-sky>
+  </a-scene>
+</body>    
+</html>
